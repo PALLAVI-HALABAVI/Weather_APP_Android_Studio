f4a846ff35335265c21d195b25510ca9f4732a2e
Pallavi <pallavihalabhavi21@gmail.com>
2024-09-08 14:51:32 +0530

    Final commit

---
+++ app/src/main/java/com/choudhary/mausamapp/InterfaceAPI.java
@@ -0,0 +1,18 @@
+package com.choudhary.mausamapp;
+
+import com.choudhary.mausamapp.Models.MausamData;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface InterfaceAPI {
+
+    @GET("weather")
+    Call<MausamData> getData(
+            @Query("q") String city,
+            @Query("appid") String API_Key,
+            @Query("units") String units
+    );
+
+}
---
+++ app/src/main/java/com/choudhary/mausamapp/MainActivity.java
@@ -0,0 +1,170 @@
+package com.choudhary.mausamapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import com.choudhary.mausamapp.Models.MausamData;
+import com.choudhary.mausamapp.Models.Weather;
+import com.choudhary.mausamapp.Models.main;
+import com.choudhary.mausamapp.databinding.ActivityMainBinding;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Weather> list;
+    ActivityMainBinding binding;
+
+    ConstraintLayout constraintLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        constraintLayout = findViewById(R.id.constraint_layout);
+
+        list = new ArrayList<>();
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/YYYY");
+        String currentDate = format.format(new Date());
+
+        binding.date.setText(currentDate);
+
+        requestPermissions();
+
+        fetchweather("Indonesia");
+
+        binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                hideKeybaord();
+
+                if (TextUtils.isEmpty(binding.searchCityEdittext.getText())){
+                    binding.searchCityEdittext.setError("Please your city name");
+                    return;
+                }
+                String CITY_NAME =binding.searchCityEdittext.getText().toString().trim();
+                fetchweather(CITY_NAME);
+                binding.searchCityEdittext.setText("");
+            }
+        });
+
+    }
+    private void hideKeybaord() {
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(constraintLayout.getApplicationWindowToken(),0);
+    }
+
+    private void requestPermissions() {
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                      )
+
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+
+                        if (multiplePermissionsReport.areAllPermissionsGranted()) {
+                            Toast.makeText(MainActivity.this, "All the permissions are granted..", Toast.LENGTH_SHORT).show();
+                        }
+                        if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied()) {
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                    }
+                }).withErrorListener(error -> {
+
+            Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT).show();
+        })
+
+                .onSameThread().check();
+    }
+
+    void fetchweather(String city){
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/data/2.5/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        InterfaceAPI interfaceAPI = retrofit.create(InterfaceAPI.class);
+        Call<MausamData>  call = interfaceAPI.getData(city,"32f564017b84535cba5c517e48098fb8","metric");
+        call.enqueue(new Callback<MausamData>() {
+            @Override
+            public void onResponse(Call<MausamData> call, Response<MausamData> response) {
+                if (response.isSuccessful()){
+                    MausamData mausamData = response.body();
+                    Log.d("Th",Thread.currentThread().getName()+" ID" +" "+ Thread.currentThread().getId());
+                    main  to = mausamData.getMain();
+
+                    binding.mainTempValue.setText((to.getTemp()) + " \u2103");
+                    binding.cityName.setText(mausamData.getName());
+                    binding.maxTempValue.setText(String.valueOf(to.getTemp_max()) + " \u2103");
+                    binding.minTempValue.setText(String.valueOf(to.getTemp_min()) + " \u2103");
+                    binding.pressreValue.setText(String.valueOf(to.getPressure())+"Pa");
+                    binding.humidityValue.setText(String.valueOf(to.getHumidity())+"g/m");
+
+                    Log.d("MV",to.getFeels_like()+" " +to.getHumidity()+" "+to.getPressure()+" "+to.getTemp());
+                    list = (ArrayList<Weather>) mausamData.getWeather();
+
+                    for( Weather  weather : list ){
+                        Log.d("MV2",weather.getId()+" "+weather.getMain()+" "+weather.getDescription());
+                       binding.description.setText(weather.getDescription());
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<MausamData> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Need Permissions");
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
+        builder.setPositiveButton("GOTO SETTINGS", (dialog, which) -> {
+            dialog.cancel();
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", getPackageName(), null);
+            intent.setData(uri);
+            startActivityForResult(intent, 101);
+        });
+        builder.setNegativeButton("Cancel", (dialog, which) -> {
+            dialog.cancel();
+        });
+        builder.show();
+    }
+}
